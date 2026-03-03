230b0afe9424378b3656080fd5f5736b10dd1bb0
Samuel Uribe Villegas <uribesamuel753@gmail.com>
2026-03-02 23:06:21 -0500

    Creación método eliminar cliente

--- src/main/java/zona_fit/datos/ClienteDAO.java
+++ src/main/java/zona_fit/datos/ClienteDAO.java
@@ -141,8 +141,27 @@ public class ClienteDAO implements IClienteDAO{
 
     @Override
     public boolean eliminarCliente(Cliente cliente) {
+        PreparedStatement ps;
+        Connection con = Conexion.getConexion();
+        var sql = "DELETE FROM cliente WHERE id = ?";
+        try{
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, cliente.getId());
+            ps.execute();
+            return true;
+        }catch(Exception e){
+            System.out.println("Error eliminando cliente");
+        }
+        finally{
+            try{
+                con.close();
+            }catch(Exception e){
+                System.out.println("Error cerrando la conexión");
+            }
+        }
         return false;
 
     }
+
 
 }
